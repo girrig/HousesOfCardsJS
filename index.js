6e449d7e96284f0ebb08f0cdd9431ba318f61161
Brandon Odom <girrig3@gmail.com>
2021-06-11 03:07:13 -0700

    Last changes to fix the webserver version of this shit
    -Fixed the static path shit in express; also added 404
    -Changed the name of housesofcards.html to index.html for easy vending with express

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = 4000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/housesofcards.html'));
+app.all('*', function(req, res) {
+  res.status(404).send('You found the catch all 404!');
 });
 
 app.listen(port, () => {
